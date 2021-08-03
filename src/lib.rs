ceac4872ceaad2417609239a5735eed00d6403a7
Charles Lew <crlf0710@gmail.com>
2021-08-03 23:09:57 +0800

    More documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,28 @@
 //! This allows all `rune`s be encoded.
 //!
 //! The `RuneString` type, is a growable rune-based string type.
+//!
+//! # Rune definition
+//!
+//! Our rune definition is based on the extended grapheme cluster defined within UAX-29.
+//! On top of this, we will convert all the CJK Compatibility Ideographs to their equivalent IVS form,
+//! and then convert the text to NFC form. We also apply a few specfic "lossy conversion" rules when
+//! necessary. The rules are defined below, and their goal to make each of the rune "standalone", that is,
+//! when two runes are put next to one each other, they won't automatically merge together into one larger
+//! rune.
+//!
+//! # Rules for lossy conversion within a rune
+//!
+//! * An orphan abstract character CR (U+000D) is converted into CR LF sequence.
+//! * If a hangul-syllable doesn't contain CHOSEONG or JUNGSEONG jamos,
+//!   corresponding filter (U+115F, U+1160) will be automatically added.
+//! * An orphan Regional Indicator (U+1F1E6..U+1F1FF) abstract character is
+//!   automatically appended another copy to make it no longer orphan.
+//! * An Extended Pictographic sequence that ends with the abstract character ZWJ (U+200D)
+//!   with an optional sequence of continuing characters before it,
+//!   will get another extra ZWJ (U+200D) abstract character to prevent it merging with next rune to
+//!   form a larger Extended Pictographic sequence.
+//! * If no base character is provided, a space (U+0020) character is inserted.
 
 pub(crate) mod tables;
 
@@ -33,7 +55,7 @@ pub use rune_str_ty::RuneStr;
 
 pub use rune_string::RuneString;
 
-/// A slice of `rune`s
+/// An slice of `rune`s
 pub type RuneSlice = [rune];
 
 /// A `Vec` of `rune`s
